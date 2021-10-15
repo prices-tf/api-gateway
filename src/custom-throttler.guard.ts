951b7e3c6163b1e49f717e5e0ab6d45a2cd8557f
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-10-15 20:32:57 +0200

    feat: throttle by ip

--- src/custom-throttler.guard.ts
+++ src/custom-throttler.guard.ts
@@ -1,9 +1,14 @@
 import { ExecutionContext, HttpException, Injectable } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
+import { FastifyRequest } from 'fastify';
 
 @Injectable()
 export class CustomThrottlerGuard extends ThrottlerGuard {
   throwThrottlingException(context: ExecutionContext) {
     throw new HttpException('Rate limit exceeded', 429);
+  }
+
+  getTracker(req: FastifyRequest): string {
+    return req.ips?.length > 0 ? req.ips[0] : req.ip;
   }
 }
