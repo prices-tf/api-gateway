339ec6a9b300feee4198dc00544b2c3940baabc6
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-10-16 03:43:34 +0200

    fix: make health checks public

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
   constructor(private health: HealthCheckService) {}
 
   @Get()
+  @Public()
   @HealthCheck()
   check() {
     return this.health.check([]);
