c3df2c80001c531b2a5d10fa27d79abbb4365744
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-10-16 18:28:15 +0200

    fix: exclude health controller from swagger

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
 import { Public } from '../auth/decorators/public.decorator';
 
+@ApiExcludeController(true)
 @Controller('health')
 export class HealthController {
   constructor(private health: HealthCheckService) {}
