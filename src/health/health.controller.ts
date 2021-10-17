1ef99ad9bea061198b5c4135466304003996566c
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-10-17 20:36:50 +0200

    fix: skip throttling health checks

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { SkipThrottle } from '@nestjs/throttler';
 import { InternalOnly } from '../auth/decorators/internal.decorator';
 import { DisableAuth } from '../auth/decorators/disable-auth.decorator';
 
 @ApiExcludeController(true)
+@SkipThrottle(true)
 @DisableAuth()
 @InternalOnly()
 @Controller('health')
