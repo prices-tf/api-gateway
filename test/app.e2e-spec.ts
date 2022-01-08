34403cc459d0a995d09cf6587190f68d869e6e5a
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-01-08 02:02:39 +0100

    test: fix problem with fastify e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,24 +2,35 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
   });
 
-  afterEach(() => {
+  afterAll(() => {
     return app.close();
   });
 
   it('should be defined', () => {
     return expect(app).toBeDefined();
+  });
+
+  it('should respond to HTTP requests', () => {
+    return request(app.getHttpServer()).get('/').expect(404);
   });
 });
