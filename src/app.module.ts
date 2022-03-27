68cd30de69d31f9c50f53bd591cc0098905db2d4
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-03-27 17:37:19 +0200

    fix: remove rate limiting
    It does not work properly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { PricesModule } from './prices/prices.module';
-import configuration, { Config } from './common/config/configuration';
+import configuration from './common/config/configuration';
 import { validation } from './common/config/validation';
 import { SnapshotsModule } from './snapshots/snapshots.module';
-import { ThrottlerModule as NestThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
-import { CustomThrottlerGuard } from './custom-throttler.guard';
-import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { ThrottlerModule } from './throttler/throttler.module';
-import { ThrottlerService } from './throttler/throttler.service';
 import { HealthModule } from './health/health.module';
 import { InternalGuard } from './internal.guard';
 import { HistoryModule } from './history/history.module';
@@ -24,37 +19,13 @@ import { HistoryModule } from './history/history.module';
       load: [configuration],
       validationSchema: validation,
     }),
-    NestThrottlerModule.forRootAsync({
-      imports: [ConfigModule, ThrottlerModule],
-      inject: [ConfigService, ThrottlerService],
-      useFactory: (
-        configService: ConfigService<Config>,
-        throttlerService: ThrottlerService,
-      ) => {
-        const throttleConfig = configService.get('throttle');
-
-        return {
-          ttl: throttleConfig.ttl,
-          limit: throttleConfig.limit,
-          storage: new ThrottlerStorageRedisService(
-            throttlerService.getRedis(),
-          ),
-        };
-      },
-    }),
     HealthModule,
     PricesModule,
     HistoryModule,
     SnapshotsModule,
     AuthModule,
-    ThrottlerModule,
   ],
   providers: [
-    {
-      // Throttle all requests
-      provide: APP_GUARD,
-      useClass: CustomThrottlerGuard,
-    },
     {
       // Require auth by default
       provide: APP_GUARD,
